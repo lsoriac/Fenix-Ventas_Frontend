7b38052178d3234c2ed84e29f4a7006b7b87a59b
Santiago Soria <lsoriac@est.ups.edu.ec>
2021-03-31 13:03:31 -0500

    Actualización Datatable

--- src/Components/ListSales.js
+++ src/Components/ListSales.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import { MDBDataTable } from 'mdbreact';
-//import { Link } from 'react-router-dom'
+import DataTable from 'react-data-table-component'
+import DataTableExtensions from 'react-data-table-component-extensions';
+import 'react-data-table-component-extensions/dist/index.css';
 
 export default class ListSales extends Component {
     state = {
         list_sales: [],
         search: '',
         params: {},
-        user_session: ''
+        user_session: '',
+        optionsPagination: { rowsPerPageText: "Filas por Página", rangeSeparatorText: "de", selectAllRowsItem: true, selectAllRowsText: "Todos" }
     }
     getSales = async () => {
         //recover from local Storage or session Storage
         if (localStorage.getItem('login') || sessionStorage.getItem('login')) {
-            let data={}
+            let data = {}
             if (localStorage.getItem('login')) {
                 data = JSON.parse(localStorage.getItem('login'))
-            }else{
+            } else {
                 data = JSON.parse(sessionStorage.getItem('login'))
             }
             //recover token
@@ -24,35 +26,35 @@ export default class ListSales extends Component {
                 token: data.token
             }
             //set user
-            this.setState({user_session: "Bienvenido "+(data.user[0].nombre)})
+            this.setState({ user_session: "Bienvenido " + (data.user[0].nombre) })
             //query to backend -  need token verify
             const res = await axios.get('http://localhost:4000/ventas', { headers })
             this.setState({ list_sales: res.data.registros })
             //style datatable
             const dat = {
                 columns: [
-                    { label: 'codcli', field: 'codcli', sort: 'asc', width: 500 },
-                    { label: 'cliente', field: 'cliente' },
-                    { label: 'emision', field: 'emision' },
-                    { label: 'vence', field: 'vence' },
-                    { label: 'totalventas', field: 'totalventas' },
+                    { name: 'codcli', selector: 'codcli', sortable: true, grow: "6" },
+                    { name: 'cliente', selector: 'cliente', sortable: true,grow: "6", minWidth: '300px', wrap: true},
+                    { name: 'emision', selector: 'emision', sortable: true },
+                    { name: 'vence', selector: 'vence', sortable: true },
+                    { name: 'totalventas', selector: 'totalventas', sortable: true, grow: "6" },
 
-                    { label: 'val', field: 'val' },
-                    { label: 'porcentajetotal', field: 'porcentajetotal' },
-                    { label: 'cantidadlog', field: 'cantidadlog' },
-                    { label: 'cienlog', field: 'cienlog' },
-                    { label: 'porcentajedv', field: 'porcentajedv' },
+                    { name: 'val', selector: 'val', sortable: true, grow: "6" },
+                    { name: 'porcentajetotal', selector: 'porcentajetotal', sortable: true, grow: "6" },
+                    { name: 'cantidadlog', selector: 'cantidadlog', sortable: true, grow: "6" },
+                    { name: 'cienlog', selector: 'cienlog', sortable: true, grow: "6" },
+                    { name: 'porcentajedv', selector: 'porcentajedv', sortable: true ,grow: "6"},
 
-                    { label: 'pndev', field: 'pndev' },
-                    { label: 'dias', field: 'dias' },
-                    { label: 'diastot', field: 'diastot' },
-                    { label: 'scoring', field: 'scoring' },
-                    { label: 'email', field: 'email' },
+                    { name: 'pndev', selector: 'pndev', sortable: true, grow: "6" },
+                    { name: 'dias', selector: 'dias' },
+                    { name: 'diastot', selector: 'diastot', sortable: true },
+                    { name: 'scoring', selector: 'scoring', sortable: true, grow: "6" },
+                    { name: 'email', selector: 'email', sortable: true, grow: "6",  minWidth: '400px', wrap: true},
 
-                    { label: 'tlf1', field: 'tlf1' },
-                    { label: 'cod_acti', field: 'cod_acti' },
-                    { label: 'nomcli2', field: 'nomcli2' },
-                    { label: 'cxc', field: 'cxc' }
+                    { name: 'tlf1', selector: 'tlf1', sortable: true, grow: "6",   wrap: true},
+                    { name: 'cod_acti', selector: 'cod_acti', sortable: true,  wrap: true, minWidth: '150px'},
+                    { name: 'nomcli2', selector: 'nomcli2', sortable: true, grow: "6",  minWidth: '300px', wrap: true },
+                    { name: 'cxc', selector: 'cxc', sortable: true, grow: "6" }
                 ],
                 rows: res.data.registros
             }
@@ -69,27 +71,42 @@ export default class ListSales extends Component {
         //obtain data
         this.getSales();
     }
-
+    
     render() {
         return (
             <div className="container p-4" style={{ minHeight: "85vh" }}>
                 <h1>VENTAS FENIX</h1>
                 <div className="table-responsive" >
-                    <div style={{textAlign:"right", color:"green"}}>
-                    <svg id="check" xmlns="http://www.w3.org/2000/svg" width="20" fill="currentColor" className="bi bi-check" viewBox="0 0 16 16">
-                                        <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                                    </svg>
-                        <label >{this.state.user_session}</label>
+                    <div style={{ textAlign: "right", color: "green" }}>
+                        <svg id="check" xmlns="http://www.w3.org/2000/svg" width="20" fill="currentColor" className="bi bi-check" viewBox="0 0 16 16">
+                            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                        </svg>
+                        <label>{this.state.user_session} </label>
                     </div>
+                    <div>
+                    <DataTableExtensions>
                     
-                    <MDBDataTable
-                        hover
-                        entriesOptions={[5, 10, 20, 25]}
-                        entries={5}
-                        striped
-                        small
-                        data={this.state.params} />
-                </div>
+                    <DataTable
+                            columns={this.state.params.columns}
+                            data={this.state.params.rows}
+                            title="Ventas"
+                            pagination
+                            striped
+                            fixedHeader
+                            responsive
+                            fixedHeaderScrollHeight="600px"
+                            paginationComponentOptions={this.state.optionsPagination}
+                        />
+
+                    </DataTableExtensions>
+  
+    
+                   
+                    </div>
+                        
+                    </div>
+
+               
             </div>
         )
     }
